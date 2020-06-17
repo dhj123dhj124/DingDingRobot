5770d860cfc44197741c297cf60e98efda648103
dhj123dhj124 <dhj123dhj124@163.com>
2020-06-17 15:29:03 +0800

    增加markdown类型

--- testSendText.py
+++ testSendText.py
@@ -57,7 +57,24 @@ def sendLink(robot,picUrl = "",messageUrl = "",receivers=[]):
     stringTextMsg = json.dumps(stringTextMsg)
     res = requests.post(getUrl(robot), data=stringTextMsg, headers=headers)
 
+#通过特定机器人向特定用户发送消息
+def sendMarkdown(robot,title = "",content = "",receivers=[]):
+    isAtAll = 0 if len(receivers)>0 else 1
+    stringTextMsg = {
+        "msgtype": "markdown",
+        "markdown": {
+        "title":title,
+        "text": content
+            },
+        "at": {
+            "atMobiles": receivers,
+            "isAtAll": isAtAll
+        }
+    }
+    stringTextMsg = json.dumps(stringTextMsg)
+    res = requests.post(getUrl(robot), data=stringTextMsg, headers=headers)
+
 if __name__ == '__main__':
-    # sendText(robots["XDD"],"康乐，你是皮痒了！",["+86-17667937371"])
     # sendText(robots["XDD"],"葫芦憨憨，你那鳖行了！",["+86-15669937729"])
-    sendLink(robots["XDD"],"","https://www.dingtalk.com/s?__biz=MzA4NjMwMTA2Ng==&mid=2650316842&idx=1&sn=60da3ea2b29f1dcc43a7c8e4a7c97a16&scene=2&srcid=09189AnRJEdIiWVaKltFzNTw&from=timeline&isappinstalled=0&key=&ascene=2&uin=&devicetype=android-23&version=26031933&nettype=WIFI")
+    # sendLink(robots["XDD"],"","https://www.dingtalk.com/s?__biz=MzA4NjMwMTA2Ng==&mid=2650316842&idx=1&sn=60da3ea2b29f1dcc43a7c8e4a7c97a16&scene=2&srcid=09189AnRJEdIiWVaKltFzNTw&from=timeline&isappinstalled=0&key=&ascene=2&uin=&devicetype=android-23&version=26031933&nettype=WIFI")
+    sendMarkdown(robots["XDD"],"杭州天气","http://www.weather.com.cn/weather/101210101.shtml")
